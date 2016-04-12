0a8ad482c45ec34210e3137a500229f6fbbe9858
Artyom Kozhemyakin <xenus.t@gmail.com>
2016-04-12 17:50:56 +0300

    Remove unnecessary dummy implicits

--- core/src/main/scala/ru/arkoit/cableguy/Resolver.scala
+++ core/src/main/scala/ru/arkoit/cableguy/Resolver.scala
@@ -5,7 +5,7 @@ import shapeless.LUBConstraint._
 import shapeless._
 
 case class Resolver[L <: HList : <<:[Provider]#λ](providers: L = HNil: HNil) {
-  def resolve[T](implicit executor: ResolverExecutor[T, serviceNoTag, Resolver[L]], d1: DummyImplicit): T = executor.resolve(this)
+  def resolve[T](implicit executor: ResolverExecutor[T, serviceNoTag, Resolver[L]]): T = executor.resolve(this)
 
-  def resolveTagged[T, TG <: ServiceTag](implicit executor: ResolverExecutor[T, TG, Resolver[L]], d1: DummyImplicit): T = executor.resolve(this)
+  def resolveTagged[T, TG <: ServiceTag](implicit executor: ResolverExecutor[T, TG, Resolver[L]]): T = executor.resolve(this)
 }
